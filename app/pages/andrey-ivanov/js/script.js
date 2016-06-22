219f41736fe7b44779eaf3fe27f0be575a133359
Andrey <andrey_ivanov_sub@mail.ru>
2016-06-22 14:38:58 +0300

    commit4

--- app/pages/andrey-ivanov/js/script.js
+++ app/pages/andrey-ivanov/js/script.js
@@ -1,7 +1,7 @@
 'user strict'
 var title = document.getElementById('title');
 var list = document.getElementById ('list');
-title.addEventListener("click", function (){
+ title.addEventListener("click", function (){
     if(!list.style.display || list.style.display=="block"){
         list.style.display="none";
     } else {
@@ -10,7 +10,7 @@ title.addEventListener("click", function (){
 });
 var title1 = document.getElementById('title1');
 var list1 = document.getElementById ('list1');
-title1.addEventListener("click", function (){
+ title1.addEventListener("click", function (){
     if(!list1.style.display || list1.style.display=="block"){
         list1.style.display="none";
     } else {
