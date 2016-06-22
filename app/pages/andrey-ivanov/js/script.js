c761330237a88493e48bde8975dc1232e401bcdc
Andrey <andrey_ivanov_sub@mail.ru>
2016-06-22 13:48:12 +0300

    commit3

---
+++ app/pages/andrey-ivanov/js/script.js
@@ -0,0 +1,19 @@
+'user strict'
+var title = document.getElementById('title');
+var list = document.getElementById ('list');
+title.addEventListener("click", function (){
+    if(!list.style.display || list.style.display=="block"){
+        list.style.display="none";
+    } else {
+        list.style.display="block";
+    }
+});
+var title1 = document.getElementById('title1');
+var list1 = document.getElementById ('list1');
+title1.addEventListener("click", function (){
+    if(!list1.style.display || list1.style.display=="block"){
+        list1.style.display="none";
+    } else {
+        list1.style.display="block";
+    }
+});
