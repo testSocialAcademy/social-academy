91df80305411ad69c3c686460fa5de5737f2d65c
Serhii <srh.kravchenko@gmail.com>
2016-06-25 23:17:58 +0300

    homework #5 ver.2.0

--- app/pages/serhii-kravchenko/js/news.js
+++ app/pages/serhii-kravchenko/js/news.js
@@ -15,55 +15,34 @@ var data = result.responseData.entries;
 
 var newsTag = document.getElementById('news');
 
-for (var i=0; i<data.length; i++){
+for (var i=0; i<data.length; i++) {
     var ul = newsTag.appendChild(document.createElement('ul'));
     ul.className = "list-group";
-    for (var key in data[i]){
-        switch (key) {
-            case "title":
-                var liTitle=ul.appendChild(document.createElement('li'));
-                    liTitle.className = "list-group-item";
-                    var aTitle = liTitle.appendChild(document.createElement('a'));
-                    aTitle.href = data[i].url;
-                    aTitle.innerHTML= data[i][key];
-                break;
-            case "contentSnippet":
-                var liContent=ul.appendChild(document.createElement('li'));
-                    liContent.className = "list-group-item";
-                    liContent.innerHTML = data[i][key];
-                break;
-            case "link":
-                var liLink=ul.appendChild(document.createElement('li'));
-                liLink.className = "list-group-item";
-                var aLink = liLink.appendChild(document.createElement('a'));
-                aLink.href = data[i][key];
-                aLink.innerHTML = data[i][key];
-                break;
-            default:
-                break;
-        }
-        // if (key == 'title'){
-        //     var liTitle=ul.appendChild(document.createElement('li'));
-        //     liTitle.className = "list-group-item";
-        //     var aTitle = liTitle.appendChild(document.createElement('a'));
-        //     aTitle.href = data[i].url;
-        //     aTitle.innerHTML= data[i][key];
-        // } else if (key=='contentSnippet') {
-        //     var liContent=ul.appendChild(document.createElement('li'));
-        //     liContent.className = "list-group-item";
-        //     liContent.innerHTML = data[i][key];
-        // } else if(key=='link'){
-        //     var liLink=ul.appendChild(document.createElement('li'));
-        //     liLink.className = "list-group-item";
-        //     var aLink = liLink.appendChild(document.createElement('a'));
-        //     aLink.href = data[i][key];
-        // } else break;
+
+    var liTitle = ul.appendChild(document.createElement('li'));
+    liTitle.className = "list-group-item";
+    var aTitle = liTitle.appendChild(document.createElement('a'));
+    aTitle.href = data[i].url;
+    aTitle.innerHTML = data[i].title;
+
+    var liContent = ul.appendChild(document.createElement('li'));
+    liContent.className = "list-group-item";
+    liContent.innerHTML = data[i].contentSnippet;
+
+    var liLink = ul.appendChild(document.createElement('li'));
+    liLink.className = "list-group-item";
+    var aLink = liLink.appendChild(document.createElement('a'));
+    aLink.href = data[i].link;
+    aLink.innerHTML = data[i].link;
+    }
+
+   
 
                 
          
-    }
     
-}
+    
+
 
 
 
