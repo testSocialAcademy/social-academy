18117992124dea9acf5120d509bfebadad64ad94
Igor N <mr.didgi@gmail.com>
2016-06-30 16:16:33 +0300

    changes from trello, except the css folder

---
+++ app/pages/igor-nevenitsa/js/script.js
@@ -0,0 +1,27 @@
+function divText()
+
+{
+
+    var a = ['Coding', 'Investing', 'Golf'];
+
+    var b = document.getElementById('hobby');
+
+    for(i=0;i<b.length;i++)
+
+    {
+        b.innerHTML = a[i];
+
+
+    }
+    for(i=0;i<a.length;i++)
+    {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(a[i]));
+        b.appendChild(li);
+    }
+    var elem = document.getElementById('hobby');
+    elem.style.background = 'lightblue';
+}
+divText()
+
+
