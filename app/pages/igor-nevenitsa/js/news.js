835ff5cec7120d2d8776f9c727f07d284092c687
Igor N <mr.didgi@gmail.com>
2016-07-13 20:16:33 +0300

    changes

--- app/pages/igor-nevenitsa/js/news.js
+++ app/pages/igor-nevenitsa/js/news.js
@@ -13,13 +13,16 @@
     if (xhr.status != 200) {
         alert(xhr.status + ": " + xhr.statusText);
     }
+        var ul = document.createElement('li');
+        myList.appendChild(li);
+        li.innerHTML = ' dffd ';
 
         var newLi;
         var resp1 = JSON.parse(xhr.responseText);
         var resp = resp1.responseData.entries;
 
 
-        for (var i = 0; i < resp.lenght; i++) {
+        for (var i = 0; i < resp.length; i++) {
             if (resp[i].title) {
                 newLi = document.createElement("li");
                 newLi.innerHTML = resp[i].title;
@@ -28,24 +31,24 @@
             }
         }
 
-        for (var i = 0; i < resp.lenght; i++) {
+        for (i = 0; i < resp.length; i++) {
             if (resp[i].contentSnippet) {
                 newLi = document.createElement("li");
                 newLi.innerHTML = resp[i].contentSnippet;
 
-                myList.children[i].appendChild(newLi);
+                myList.children[0].appendChild(newLi);
             }
         }
 
-        for (var i = 0; i < resp.lenght; i++) {
+        for (i = 0; i < resp.length; i++) {
             if (resp[i].link) {
                 var link = document.createElement("a");
                 link.setAttribute("href", resp[i].link);
                 link.innerHTML = resp[i].link;
                 newLi = document.createElement("li");
                 newLi.innerHTML = "";
-                myList.children[i].appendChild(newLi);
-                myList.children[i].lastChild.appendChild(link);
+                myList.children[0].appendChild(newLi);
+                myList.children[0].lastChild.appendChild(link);
             }
         }
 
