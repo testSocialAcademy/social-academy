85c0450400dc9b8401aa892db7ecc7913ca468f0
Slava Nehliad <ashtubee@gmail.com>
2016-07-14 20:40:40 +0300

    test version (hw5)

--- app/pages/slava-nehliad/js/script-nehliad.js
+++ app/pages/slava-nehliad/js/script-nehliad.js
@@ -1,20 +1,61 @@
 "use strict";
 (function() {
-
-
-        function showHobby() {
-            var personalInfo = document.getElementById("hobby-list");
-            var list = document.createElement("ul");
-            var hobby = ["music", "literature", "programming", "basket-ball"];
-            for (var i = 0; i < hobby.length; i++){
-                var li = document.createElement("li");
-                li.innerHTML = hobby[i];
-                list.appendChild(li);
+//      Function add hobby
+    
+    function showHobby() {
+        var personalInfo = document.getElementById("hobby-list");
+        var list = document.createElement("ul");
+        var hobby = ["music", "literature", "programming", "basket-ball"];
+           for (var i = 0; i < hobby.length; i++){
+               var li = document.createElement("li");
+               li.innerHTML = hobby[i];
+               list.appendChild(li);
             }
             
-             personalInfo.appendChild(list);
+            personalInfo.appendChild(list);
         }
-        showHobby()
+
+    showHobby();
+
+
+//      Function add news
+        
+    function loadNews() {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "news.json", false);
+        xhr.send();
+
+        alert( xhr.responseText );
+
+        // xhr.onreadystatechange = function () {
+        //     if (xhr.readyState != 4) return;
+        // };
+
+        if (xhr.status != 200) {
+            alert(xhr.status + ": " + xhr.statusText);
+        } else {
+            setNews(xhr.responseText);
+        }
+
+        var newsList = document.getElementById("newsList");
+
+        function setNews(responseText) {
+            var newTitle;
+            var resp = JSON.parse(responseText);
+
+
+            for (var i = 0; i < resp.length; i++) {
+                if(resp[i].title) {
+                    newTitle = document.createElement("h3");
+                    newTitle.innerHTML = resp[i].title;
+                    newsList.appendChild(newTitle);
+                }
+            }
+        }
+    }
+    loadNews();
+    
 })();
 
 
