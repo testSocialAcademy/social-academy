dc6fb01f2dd0f20c9f357bcf36f72b3ec0587146
Andrey <andrey_ivanov_sub@mail.ru>
2016-07-25 13:00:16 +0300

    Homework_8

--- app/pages/andrey-ivanov/js/tests/test.js
+++ app/pages/andrey-ivanov/js/tests/test.js
@@ -4,8 +4,25 @@ var expect = chai.expect;
 
 
 
-   
+describe("Результаты тестирования", function () {
+    describe("getHobby()", function() {
+        it("Функция возвращает массив", function (){
+            expect(getHobby()).to.be.a('Array');
+        });
+        it("массив не пустой", function (){
+            expect(getHobby()).to.be.not.empty;
+        });
 
+    });
+    describe("newsDisplay(responseText)", function() {
+        it("массив", function (){
+           // expect(newsDisplay( )).to.be.arguments;
+
+        });
+
+    });
+
+});
 
 
 mocha.run();
