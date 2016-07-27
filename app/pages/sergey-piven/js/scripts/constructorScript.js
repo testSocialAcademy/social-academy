df3879d32186bfb55212ebb6b4483a364eed8b64
PivenSergey <pikipen7@gmail.com>
2016-07-27 21:51:55 +0300

    'homework9'

--- app/pages/sergey-piven/js/scripts/constructorScript.js
+++ app/pages/sergey-piven/js/scripts/constructorScript.js
@@ -4,7 +4,7 @@ String.prototype.createPhone = function () {
         if (isNaN(arr[i])) {
             alert("Error: Неправильный формат");
             break;
-        } else if (i%4 == 0) {
+        } else if (i % 4 == 0) {
             arr.splice(i, 0, "-");
         }
     }
@@ -17,99 +17,112 @@ var malesList = document.getElementById("malesByGender");
 var femalesList = document.getElementById("femalesByGender");
 var li;
 
-var objForTest = {
-    name: "Egor",        //Я создал этот объект что бы протестить вызов метода getSet с параметром
-    gender: "male"
-};
+/*var objForTest = {
+ name: "Egor",
+ gender: "male"
+ };*/
 
 function Users() {
-    var _this = this;
     this.male = {};
     this.female = {};
     this._people = {};
-    this.getAjax = function (link) {
-        var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-        var xhr = new XHR();
-
-        xhr.open('GET', link, true);
-
-        xhr.onload = function () {
-            _this._people = JSON.parse(this.responseText);
-            callback();
-        };
-
-        xhr.onerror = function () {
-            alert('Ошибка ' + this.status);
-        };
-
-        xhr.send();
-    };
 }
+
+Users.prototype = Object.create(Object.prototype);
+Users.prototype.constructor = Users;
+
+
+Users.prototype.getAjax = function (link) {
+    var _this = this;
+    var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+    var xhr = new XHR();
+
+    xhr.open('GET', link, true);
+
+    xhr.onload = function () {
+        _this._people = JSON.parse(this.responseText);
+        callback();
+    };
+
+    xhr.onerror = function () {
+        alert('Ошибка ' + this.status);
+    };
+
+    xhr.send();
+};
 
 function NewUsers() {
     var _this = this;
     this.creator = "Serega";
     this.dateOfCreation = new Date();
-    this.sortPeopleByGender = function () {
-        for (var i = 0; i < _this._people.results.length; i++) {
-            if (_this._people.results[i].gender == "female") {
-                _this.female[i] = _this._people.results[i];
-            } else if (_this._people.results[i].gender == "male") {
-                _this.male[i] = _this._people.results[i];
-            }
-        }
-    };
-    this.getSet = function (person) {
-        if(!arguments.length) {
-            return _this._people;
-        } else if (person.gender == "female") {
-            _this.female.newPerson = person;
-        } else if (person.gender == "male") {
-            _this.male.newPerson = person;
-        }
-    };
-
-    this.getSetDisplayOnPageByGender = function () {
-        _this.getSet();
-        for (var i = 0; i < _this._people.results.length; i ++) {
-            if (_this._people.results[i].gender == "male") {
-                li = document.createElement('li');
-                li.innerHTML = _this._people.results[i].name.title + " " + _this._people.results[i].name.first + " " +
-                    _this._people.results[i].name.last;
-                malesList.appendChild(li);
-
-                li = document.createElement('li');
-                li.innerHTML = _this._people.results[i].email;
-                malesList.appendChild(li);
-
-                li = document.createElement('hr');
-                malesList.appendChild(li);
-            } else if (_this._people.results[i].gender == "female") {
-                li = document.createElement('li');
-                li.innerHTML = _this._people.results[i].name.title + " " + _this._people.results[i].name.first + " " +
-                    _this._people.results[i].name.last;
-                femalesList.appendChild(li);
-
-                li = document.createElement('li');
-                li.innerHTML = _this._people.results[i].email;
-                femalesList.appendChild(li);
-
-                li = document.createElement('hr');
-                femalesList.appendChild(li);
-            }
-        }
-    };
     Users.apply(this, arguments);
 }
+
+NewUsers.prototype = Object.create(Users.prototype);
+NewUsers.prototype.constructor = NewUsers;
+
+NewUsers.prototype.sortPeopleByGender = function () {
+    var _this = this;
+    for (var i = 0; i < _this._people.results.length; i++) {
+        if (_this._people.results[i].gender == "female") {
+            _this.female[i] = _this._people.results[i];
+        } else if (_this._people.results[i].gender == "male") {
+            _this.male[i] = _this._people.results[i];
+        }
+    }
+};
+
+NewUsers.prototype.getSet = function (person) {
+    var _this = this;
+    if (!arguments.length) {
+        return _this._people;
+    } else if (person.gender == "female") {
+        _this.female.newPerson = person;
+    } else if (person.gender == "male") {
+        _this.male.newPerson = person;
+    }
+};
+
+NewUsers.prototype.getSetDisplayOnPageByGender = function () {
+    var _this = this;
+    for (var i = 0; i < _this._people.results.length; i++) {
+        if (_this._people.results[i].gender == "male") {
+            li = document.createElement('li');
+            li.innerHTML = _this._people.results[i].name.title + " " + _this._people.results[i].name.first + " " +
+                _this._people.results[i].name.last;
+            malesList.appendChild(li);
+
+            li = document.createElement('li');
+            li.innerHTML = _this._people.results[i].email;
+            malesList.appendChild(li);
+
+            li = document.createElement('hr');
+            malesList.appendChild(li);
+        } else if (_this._people.results[i].gender == "female") {
+            li = document.createElement('li');
+            li.innerHTML = _this._people.results[i].name.title + " " + _this._people.results[i].name.first + " " +
+                _this._people.results[i].name.last;
+            femalesList.appendChild(li);
+
+            li = document.createElement('li');
+            li.innerHTML = _this._people.results[i].email;
+            femalesList.appendChild(li);
+
+            li = document.createElement('hr');
+            femalesList.appendChild(li);
+        }
+    }
+};
 
 var dudes = new NewUsers();
 
 dudes.getAjax("http://api.randomuser.me/?results=10");
 
-function callback() {                                       //Тут я делал тестовое создание объекта дополнительным конструктором, проверку методов
-    dudes.sortPeopleByGender();                             //включая тот который делает вывод на страницу
+function callback() {
+    dudes.sortPeopleByGender();
     /*dudes.getSet(objForTest);*/
     dudes.getSetDisplayOnPageByGender();
     console.log(dudes);
 }
+
 
