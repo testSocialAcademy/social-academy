2d224552888989d41ff2b1e77e6fe6e005bf7104
Serhii <srh.kravchenko@gmail.com>
2016-08-17 14:35:58 +0300

    lesson#9

--- app/pages/serhii-kravchenko/js/scripts/users.js
+++ app/pages/serhii-kravchenko/js/scripts/users.js
@@ -1,70 +1,95 @@
 'use strict';
 function Users() {
-    var self = this;
-    self.usersData={};
+    this.usersData={};
 
-    function users() {
-        xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://api.randomuser.me/?results=10", false);
-        xhr.send();
-        if (xhr.status != 200) {
-            alert(xhr.status + " " + xhr.statusText);
-        } else {
-            var allUsers = JSON.parse(xhr.responseText);
-            self.usersData = allUsers.results;
-        }
-    }
-
-    this.getUsers = function () {
-        users();
-        return self.usersData;
-    };
 }
+Users.prototype.users = function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://api.randomuser.me/?results=10", false);
+    xhr.send();
+    if (xhr.status != 200) {
+        alert(xhr.status + " " + xhr.statusText);
+    } else {
+        var allUsers = JSON.parse(xhr.responseText);
+        this.usersData = allUsers.results;
+    }
+};
+Users.prototype.getUsers = function () {
+    Users.prototype.users();
+    return this.usersData;
+};
 
 function GenderUsers() {
-    Users.apply(this, arguments);
-    self.males = [];
-    self.females = [];
-    var users = this.getUsers();
+    this.males = [];
+    this.females = [];
+}
+GenderUsers.prototype = Object.create(Users.prototype);
+GenderUsers.prototype.constructor = GenderUsers;
 
-    this.sortUsersByGender = function () {
-        for (var i = 0; i < users.length; i++) {
-            if (users[i].gender == "male") {
-                self.males.push(users[i])
-            } else if (users[i].gender == "female") {
-                self.females.push(users[i])
-            }
+GenderUsers.prototype.sortUsersByGender = function () {
+    for (var i = 0; i < this.users.length; i++) {
+        if (this.users[i].gender == "male") {
+            this.males.push(this.users[i])
+        } else if (this.users[i].gender == "female") {
+            this.females.push(this.users[i])
         }
-    };
-
-    this.getOrSetUser = function (person) {
-        if (!arguments.length) {
-            return users;
-        } else if (person.gender == "male") {
-            self.males.push(person)
-        } else if (person.gender == "female") {
-            self.females.push(person)
-        }
-    };
-    this.displayOnPageUsersByGender = function () {
-        var usersToDisplay = this.getOrSetUser();
-        var ulBoys = document.getElementById('boys');
-        var ulGirls = document.getElementById('girls');
-        if (ulBoys) {
-            for (var i = 0; i < usersToDisplay.length; i++) {
-                if (usersToDisplay[i].gender == "male") {
-                    var liBoys = ulBoys.appendChild(document.createElement('li'));
-                    liBoys.className = "list-group-item";
-                    liBoys.innerHTML = usersToDisplay[i].name.first.charAt(0).toUpperCase() + usersToDisplay[i].name.first.slice(1) + " " + usersToDisplay[i].name.last.charAt(0).toUpperCase() + usersToDisplay[i].name.last.slice(1);
-                } else if (usersToDisplay[i].gender == "female") {
-                    var liGirls = ulGirls.appendChild(document.createElement('li'));
-                    liGirls.className = "list-group-item";
-                    liGirls.innerHTML = usersToDisplay[i].name.first.charAt(0).toUpperCase() + usersToDisplay[i].name.first.slice(1) + " " + usersToDisplay[i].name.last.charAt(0).toUpperCase() + usersToDisplay[i].name.last.slice(1);
-                }
+    }
+};
+GenderUsers.prototype.getOrSetUser = function (person) {
+    if (!arguments.length) {
+        return this.getUsers();
+    } else if (person.gender == "male") {
+        this.males.push(person);
+    } else if (person.gender == "female") {
+        this.females.push(person);
+    }
+};
+GenderUsers.prototype.displayOnPageUsersByGender = function () {
+    var usersToDisplay = this.getOrSetUser();
+    var ulBoys = document.getElementById('boys');
+    var ulGirls = document.getElementById('girls');
+    if (ulBoys) {
+        for (var i = 0; i < usersToDisplay.length; i++) {
+            if (usersToDisplay[i].gender == "male") {
+                var liBoys = ulBoys.appendChild(document.createElement('li'));
+                liBoys.className = "list-group-item";
+                liBoys.innerHTML = usersToDisplay[i].name.first.charAt(0).toUpperCase() + usersToDisplay[i].name.first.slice(1) + " " + usersToDisplay[i].name.last.charAt(0).toUpperCase() + usersToDisplay[i].name.last.slice(1);
+            } else if (usersToDisplay[i].gender == "female") {
+                var liGirls = ulGirls.appendChild(document.createElement('li'));
+                liGirls.className = "list-group-item";
+                liGirls.innerHTML = usersToDisplay[i].name.first.charAt(0).toUpperCase() + usersToDisplay[i].name.first.slice(1) + " " + usersToDisplay[i].name.last.charAt(0).toUpperCase() + usersToDisplay[i].name.last.slice(1);
             }
         }
     }
-}
+};
 
 var gender = new GenderUsers();
 gender.displayOnPageUsersByGender();
+
+
+String.prototype.phoneParser = function(){
+    var count = 0;
+    var result = "";
+    for (var i=0; i<this.length;i++){
+        if (count%3==0 && count!==0){
+            result+="-"
+        }
+        if(!isNaN(this)){
+            count++;
+            result+=this[i];
+        }
+        else {
+            return "Неправильный формат"
+        }
+
+    }
+    return result;
+};
+
+var result = "9873216549".phoneParser();
+
+console.log(result); //987-321-654-9
+
+var result1 = "9873216549nnn".phoneParser();
+
+console.log(result1); // Error: Неправильный формат
