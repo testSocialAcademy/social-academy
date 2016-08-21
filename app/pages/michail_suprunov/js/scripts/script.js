f6ac86cfa88c81960f6d74601a854ba188d5c537
Michail <michaildas@mail.ru>
2016-08-21 15:59:47 +0300

    Added prefixes

--- app/pages/michail_suprunov/js/scripts/script.js
+++ app/pages/michail_suprunov/js/scripts/script.js
@@ -1,18 +1,18 @@
 ;'use strict';
-//(function(){
+(function(){
 //Display hobbies	
-function displayHobbies() {
-    var div = document.getElementById("personal-data");
+function displayHobbies_mu() {
+    var div = document.getElementById("personal-data_mu");
     var pText;
     var p = document.createElement('p');
-    pText = formHobbies();
+    pText = formHobbies_mu();
     p.innerHTML = pText;
     if (div) {
         div.appendChild(p);
     }
     return 1;
 }
-function formHobbies() {
+function formHobbies_mu() {
     var hobbies = ["literature", "bodybuilding", "art", "walking"];
     var pText = "Hobbies: ";
     for (var i = 0; i < hobbies.length; i++) {
@@ -23,11 +23,11 @@ function formHobbies() {
 }
 
 //Functions for news
-function displayNews() {
+function displayNews_mu() {
     var response = null;
-    response = newsRequest();
+    response = newsRequest_mu();
     if (response != null) {
-        putNewsOnPage(response);
+        putNewsOnPage_mu(response);
     }
     else {
         return false;
@@ -35,7 +35,7 @@ function displayNews() {
 
 }
 
-function newsRequest() {
+function newsRequest_mu() {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "news.json", false);
     xhr.send();
@@ -49,19 +49,19 @@ function newsRequest() {
     }
 }
 
-function putNewsOnPage(responseText) {
+function putNewsOnPage_mu(responseText) {
     var response = JSON.parse(responseText);
     var news = response.responseData.entries;
-    var column = document.getElementById("newsColumn");
+    var column = document.getElementById("newsColumn_mu");
     var div;
     var obj;
     for (var i = 0; i < news.length; i++) {
         obj = news[i];
-        div = formElement();
+        div = formElement_mu();
         column.appendChild(div);
     }
 
-    function formElement() {
+    function formElement_mu() {
         var p;
         var a;
         var div = document.createElement("div");
@@ -84,24 +84,24 @@ function putNewsOnPage(responseText) {
     }
 }
 //Work with Local Storage
-function addElementLSList() {
-    var keyLS = document.getElementById("keyLS");
+function addElementLSList_mu() {
+    var keyLS = document.getElementById("keyLS_mu");
     var value = keyLS.value;
     var result = localStorage.getItem(value);
     if ((localStorage.length == 0 || result == null) && (value != "")) {
-        addElementLS();
-        addElementList();
+        addElementLS_mu();
+        addElementList_mu();
     }
 
-    function addElementLS() {
+    function addElementLS_mu() {
         localStorage.setItem(value, value);
     }
 
-    function addElementList() {
+    function addElementList_mu() {
         var li = document.createElement("li");
-        var listLS = document.getElementById("listLS");
+        var listLS = document.getElementById("listLS_mu");
         li.textContent = value;
-        li.onclick = removeLi;
+        li.onclick = removeLi_mu;
         listLS.appendChild(li);
         keyLS.value = "";
         return "very nice";
@@ -109,40 +109,40 @@ function addElementLSList() {
 }
 
 
-function removeLi() {
+function removeLi_mu() {
     var _this = this;
-    removeLiLS();
-    removeLiPage();
+    removeLiLS_mu();
+    removeLiPage_mu();
 
-    function removeLiLS() {
+    function removeLiLS_mu() {
         localStorage.removeItem(_this.textContent);
     }
 
-    function removeLiPage() {
-        var listLS = document.getElementById("listLS");
+    function removeLiPage_mu() {
+        var listLS = document.getElementById("listLS_mu");
         listLS.removeChild(_this);
     }
 }
 
-function displayLS() {
+function displayLS_mu() {
     var li = document.createElement("li");
     var value;
     if (value) {
-        value = document.getElementById("keyLS").value;
+        value = document.getElementById("keyLS_mu").value;
     }
-    var listLS = document.getElementById("listLS");
+    var listLS = document.getElementById("listLS_mu");
     for (var i = 0; i < localStorage.length; i++) {
         li.textContent = localStorage.key(i);
-        li.onclick = removeLi;
+        li.onclick = removeLi_mu;
         listLS.appendChild(li);
         li = document.createElement("li");
     }
 }
 // Work with Users
-function Users() {
+function Users_mu() {
     this._users = null;
 }
-Users.prototype.getUsers = function (usersSource) {
+Users_mu.prototype.getUsers = function (usersSource) {
     var response;
 
     response = this._usersRequest(usersSource);
@@ -152,7 +152,7 @@ Users.prototype.getUsers = function (usersSource) {
         this._users = response;
     }
 };
-Users.prototype._usersRequest = function (source) {
+Users_mu.prototype._usersRequest = function (source) {
     var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
     var xhr = new XHR();
     var responseObject;
@@ -169,16 +169,16 @@ Users.prototype._usersRequest = function (source) {
     return responseObject;
 };
 
-function SexUsers() {
+function SexUsers_mu() {
     this._girls = [];
     this._boys = [];
     this._div = {};
-    Users.apply(this, arguments);
+    Users_mu.apply(this, arguments);
 }
-SexUsers.prototype = Object.create(Users.prototype);
-SexUsers.prototype.constructor = SexUsers;
+SexUsers_mu.prototype = Object.create(Users_mu.prototype);
+SexUsers_mu.prototype.constructor = SexUsers_mu;
 
-SexUsers.prototype.sortUsers = function () {
+SexUsers_mu.prototype.sortUsers = function () {
     for (var i = 0; i < this._users.length; i++) {
         if (this._users[i].gender == 'male') {
             this._boys.push(this._users[i]);
@@ -188,7 +188,7 @@ SexUsers.prototype.sortUsers = function () {
     }
 };
 
-SexUsers.prototype.getSetUsers = function (newUser) {
+SexUsers_mu.prototype.getSetUsers = function (newUser) {
     var response;
 
     if (arguments.length > 0) {
@@ -204,7 +204,7 @@ SexUsers.prototype.getSetUsers = function (newUser) {
     }
 };
 
-SexUsers.prototype._addUser = function (newUser) {
+SexUsers_mu.prototype._addUser = function (newUser) {
     this._users.push(newUser);
     if (newUser.gender == 'male') {
         this._boys.push(newUser);
@@ -213,21 +213,21 @@ SexUsers.prototype._addUser = function (newUser) {
     }
 };
 
-SexUsers.prototype.display = function () {
+SexUsers_mu.prototype.display = function () {
     for (var i = 0; i < this._users.length; i++) {
         this._div = this._formElement(i);
         this._appendElement(this._div, i);
     }
 };
 
-SexUsers.prototype._formElement = function (i) {
+SexUsers_mu.prototype._formElement = function (i) {
     var div;
     var img;
     var p;
     div = document.createElement("div");
     img = document.createElement("img");
     p = document.createElement("p");
-    img.className = "img-responsive userImage";
+    img.className = "img-responsive userImage_mu";
     img.src = this._users[i].picture.large;
     p.textContent = this._users[i].name.first + " " + this._users[i].name.last;
     div.appendChild(img);
@@ -235,10 +235,10 @@ SexUsers.prototype._formElement = function (i) {
     return div;
 };
 
-SexUsers.prototype._appendElement = function (div, i) {
-    var maleUsers = document.getElementById("maleUsers");
-    var femaleUsers = document.getElementById("femaleUsers");
-    var friendsUsers = document.getElementById("friendsUsers");
+SexUsers_mu.prototype._appendElement = function (div, i) {
+    var maleUsers = document.getElementById("maleUsers_mu");
+    var femaleUsers = document.getElementById("femaleUsers_mu");
+    var friendsUsers = document.getElementById("friendsUsers_mu");
     if (this._users[i].gender == "male") {
         if (maleUsers) {
             maleUsers.appendChild(div);
@@ -257,7 +257,7 @@ SexUsers.prototype._appendElement = function (div, i) {
 };
 //HOMEWORK 9
 
-function PhoneError(type) {
+function PhoneError_mu(type) {
     this.name = "PhoneError";
     this.message = "Неправильный формат." + type;
     if (Error.captureStackTrace) {
@@ -267,18 +267,18 @@ function PhoneError(type) {
     }
 
 }
-PhoneError.prototype = Object.create(Error.prototype);
-PhoneError.prototype.constructor = PhoneError;
+PhoneError_mu.prototype = Object.create(Error.prototype);
+PhoneError_mu.prototype.constructor = PhoneError_mu;
 
 
-String.prototype.createPhone = function () {
+String.prototype.createPhone_mu = function () {
     var i;
     var result = "";
     if (this.search(/\D/) != -1) {
-        throw new PhoneError("Вводите только цифры");
+        throw new PhoneError_mu("Вводите только цифры");
     }
     if (this.length != 10) {
-        throw new PhoneError("Цифр должно быть 10");
+        throw new PhoneError_mu("Цифр должно быть 10");
     }
     for (i = 0; i < this.length; i++) {
         if (i != 0 && !(i % 3)) {
@@ -290,10 +290,10 @@ String.prototype.createPhone = function () {
 };
 
 try {
-    var result = "1231231231".createPhone();  //Пробуем здесь разные входные данные
-    alert(result);
+    var result = "1231231231".createPhone_mu();  //Пробуем здесь разные входные данные
+    console.log(result);
 } catch (err) {
-    if (err instanceof PhoneError) {
+    if (err instanceof PhoneError_mu) {
         alert(err.message);
     } else {
         throw err;
@@ -301,24 +301,24 @@ try {
 }
 
 //main body///////////////
-function workWithUsers() {
+function workWithUsers_mu() {
     var usersSource = "http://api.randomuser.me/?results=10";
-    var users = new SexUsers();
+    var users = new SexUsers_mu();
     users.getUsers(usersSource);
     users.sortUsers();
     users.display();
 }
 
-function workWithLS() {
-    var submitInputLS = document.getElementById("submitInputLS");
+function workWithLS_mu() {
+    var submitInputLS = document.getElementById("submitInputLS_mu");
     if (submitInputLS) {
-        submitInputLS.addEventListener("click", addElementLSList);
+        submitInputLS.addEventListener("click", addElementLSList_mu);
     }
-    displayLS();
+    displayLS_mu();
 }
 
-workWithUsers();
-workWithLS();
-displayHobbies();
-displayNews();
-//})();
+workWithUsers_mu();
+workWithLS_mu();
+displayHobbies_mu();
+displayNews_mu();
+})();
