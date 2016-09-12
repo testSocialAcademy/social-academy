39b09f1a2b121d66060c0feda784e34641ce3554
kirjam <shmkirill@gmail.com>
2016-09-12 14:03:02 +0300

    comm

--- app/pages/kirill_shmetan/js/scripts/script.js
+++ app/pages/kirill_shmetan/js/scripts/script.js
@@ -1,10 +1,10 @@
-function hobbyOpen () {
+'use strict';
 
     var elem = document.getElementById('hobbies');
 
     var hobbie = ['guitar playing', 'football', 'running', 'road cycling', 'gym', 'literature'];
 
-    
+    function hobbyOpen () {
     for (var i = 0; i < hobbie.length; i++) {
         var newLi = document.createElement('li');
         newLi.innerHTML = hobbie[i] + ';';
