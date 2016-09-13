b810f9c1946720dc08d1ef1b7e31f4849cb8757f
Slava Nehliad <ashtubee@gmail.com>
2016-09-13 03:27:44 +0300

    add HW 9

--- app/pages/slava-nehliad/js/scripts/script-nehliad.js
+++ app/pages/slava-nehliad/js/scripts/script-nehliad.js
@@ -1,7 +1,7 @@
 ;"use strict";
 
 //      Function add hobby
-    
+
     function showHobby() {
         var li;
         var personalInfo = document.getElementById("hobby-list");
@@ -23,7 +23,7 @@
 
 
 //      Function add news
-        
+
     function loadNews() {
 
         var xhr = new XMLHttpRequest();
@@ -41,13 +41,13 @@
         }
     }
 
-        
+
         function setNews(responseText) {
             var newTitle;
             var newPost;
             var newsList = document.getElementById("newsList");
             var resp = JSON.parse(responseText);
-            
+
             for (var i = 0; i < resp.responseData.entries.length; i++) {
                 newPost = document.createElement("div");
                 newPost.setAttribute("class", "newsBlock");
@@ -76,8 +76,8 @@
 
     }
     loadNews();
-    
-    
+
+
 //    Function add items to "ToDoList" and Local Storage
 
     function loadItems() {
@@ -124,7 +124,7 @@
 
 
 /////////////////////////////////////////////////////////////
-//      HOMEWORK 8
+//      HOMEWORK 8, 9
 /////////////////////////////////////////////////////////////
 
 var li;
@@ -132,78 +132,93 @@ var manList = document.getElementById("male");
 var womanList = document.getElementById("female");
 
 function Users() {
-    var _this = this;
+    //var _this = this;
     this.man = {};
     this.woman = {};
     this.people = {};
-    this.getUsers = function (link) {
-        var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-        var xhr = new XHR();
-
-        xhr.open('GET', link, true);
-
-        xhr.onload = function () {
-            _this.people = JSON.parse(this.responseText);
-            startFunc();
-        };
-
-        xhr.onerror = function () {
-            alert('Ошибка ' + this.status);
-        };
-
-        xhr.send();
-    };
 }
+
+Users.prototype = Object.create(Object.prototype);
+Users.prototype.constructor = Users;
+
+Users.prototype.getUsers = function (link) {
+    var _this = this;
+
+    var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+    var xhr = new XHR();
+
+    xhr.open('GET', link, true);
+
+    xhr.onload = function () {
+        _this.people = JSON.parse(this.responseText);
+        startFunc();
+    };
+
+    xhr.onerror = function () {
+        alert('Ошибка ' + this.status);
+    };
+
+    xhr.send();
+};
+
 
 function NewUsers() {
     var _this = this;
-
-    this.sortByGender = function () {
-        for (var i = 0; i < _this.people.results.length; i++) {
-            if (_this.people.results[i].gender == "female") {
-                _this.woman[i] = _this.people.results[i];
-            } else if (_this.people.results[i].gender == "male") {
-                _this.man[i] = _this.people.results[i];
-            }
-        }
-    };
-
-    this.postUsers = function () {
-        
-        for (var i = 0; i < _this.people.results.length; i ++) {
-            
-            if (_this.people.results[i].gender == "male") {
-                li = document.createElement('li');
-                li.innerHTML = _this.people.results[i].name.title + " " + _this.people.results[i].name.first + 
-                    " " + _this.people.results[i].name.last;
-                manList.appendChild(li);
-
-                li = document.createElement('li');
-                li.innerHTML = _this.people.results[i].email;
-                manList.appendChild(li);
-
-                li = document.createElement('br');
-                manList.appendChild(li);
-            }
-            
-            if (_this.people.results[i].gender == "female") {
-                li = document.createElement('li');
-                li.innerHTML = _this.people.results[i].name.title + " " + _this.people.results[i].name.first +                    " " + _this.people.results[i].name.last;
-                womanList.appendChild(li);
-
-                li = document.createElement('li');
-                li.innerHTML = _this.people.results[i].email;
-                womanList.appendChild(li);
-
-                li = document.createElement('br');
-                womanList.appendChild(li);
-            }
-            
-        }
-    };
-
     Users.apply(this, arguments);
 }
+
+NewUsers.prototype = Object.create(Users.prototype);
+NewUsers.prototype.constructor = NewUsers;
+
+NewUsers.prototype.sortByGender = function () {
+    var _this = this;
+
+    for (var i = 0; i < _this.people.results.length; i++) {
+        if (_this.people.results[i].gender == "female") {
+                this.woman[i] = _this.people.results[i];
+        } else if (_this.people.results[i].gender == "male") {
+                _this.man[i] = _this.people.results[i];
+        }
+    }
+};
+
+NewUsers.prototype.postUsers = function () {
+    var _this = this;
+
+    for (var i = 0; i < _this.people.results.length; i ++) {
+
+        if (_this.people.results[i].gender == "male") {
+            li = document.createElement('li');
+            li.innerHTML = _this.people.results[i].name.title + " " + _this.people.results[i].name.first +
+                " " + _this.people.results[i].name.last;
+            manList.appendChild(li);
+
+            li = document.createElement('li');
+            li.innerHTML = _this.people.results[i].email;
+            manList.appendChild(li);
+
+            li = document.createElement('br');
+            manList.appendChild(li);
+        }
+
+        if (_this.people.results[i].gender == "female") {
+            li = document.createElement('li');
+            li.innerHTML = _this.people.results[i].name.title + " " + _this.people.results[i].name.first +
+                " " + _this.people.results[i].name.last;
+            womanList.appendChild(li);
+
+            li = document.createElement('li');
+            li.innerHTML = _this.people.results[i].email;
+            womanList.appendChild(li);
+
+            li = document.createElement('br');
+            womanList.appendChild(li);
+        }
+
+    }
+};
+
+//Users.apply(this, arguments);
 
 var newUsers = new NewUsers();
 
@@ -214,6 +229,28 @@ function startFunc() {
     newUsers.postUsers();
 }
 
+String.prototype.createPhone = function () {
+    var arr = this.split('');
+
+    for (var i = 0; i < arr.length; i++) {
+        // if (!(typeof arr == "number")) {
+        //     alert("Error: Неправильный формат!");
+        //     break;
+        if (isNaN(arr[i])) {
+            alert("Error: Неправильный формат!");
+            break;
+        } else if (i % 4 == 0) {
+            arr.splice(i, 0, "-");
+        }
+    }
+
+    if (i == arr.length) {
+        arr.shift();
+        var result = arr.join('');
+        return result;
+    }
+};
+//console.log("4545454545".createPhone());
     
     
     
